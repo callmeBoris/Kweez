c52931dfe20588afb90b0f9c5af3714c32154003
callmeBoris <boriskayienzo@gmail.com>
2019-12-01 19:08:04 +0200

    removed the debug bar

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
           appBar: AppBar(
             title: Text('Kweez'),
